c1a1bfc47f1146150feec494aba71794f5755708
Dorofeev-Aleksey <Mr.dorick@yandex.ru>
2017-05-02 14:10:52 +0300

    Исправление ошибок 1

--- js/technomart.js
+++ js/technomart.js
@@ -1,4 +1,28 @@
-var link = document.querySelector(".popup-btn");
+      var order_btn = document.querySelectorAll(".buy");
+      var order_popup = document.querySelector(".add-item");
+      var order_close = document.querySelector(".modal-content-close");
+      
+      for (var i = order_btn.length - 1; i >= 0; i--) {
+          order_btn[i].addEventListener("click", function(event){
+            event.preventDefault();
+            order_popup.classList.add("modal-content-show");
+        });
+      }
+
+      order_close.addEventListener("click", function(event){
+        event.preventDefault();
+        order_popup.classList.remove("modal-content-show");
+      });
+
+      window.addEventListener("keydown", function(event){
+        if (event.keyCode === 27) {
+          if (order_popup.classList.contains("modal-content-show")) {
+            order_popup.classList.remove("modal-content-show");
+          }
+        }
+      });   
+
+      var link = document.querySelector(".popup-btn");
       var popup = document.querySelector(".modal-content");
       var close = popup.querySelector(".modal-content-close");
       var namefield = popup.querySelector("[name=name]");
